ab765ab9c5097d59bd3639e480dd8bd8f06e4402
Anastasiia Kovalchuk <keshka3455@gmail.com>
2023-03-10 14:20:53 +0200

    edited some files for laboratory worl

--- com/company/OSandOP/lab2/lab2.py
+++ com/company/OSandOP/lab2/lab2.py
@@ -1,102 +1,112 @@
-import os, glob
+import os
+import glob
 from datetime import datetime
 
 
-def log(logTxt):
-    now = datetime.now()
-    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")  # get today's day
+# отримуємо сьогоднішній день та записуємо у файл
+def get_day_and_write_to_log_file(logTxt):
+    today = datetime.now()
+    dt_string = today.strftime("%d/%m/%Y %H:%M:%S")
     with open("information_for_lab_2.txt", 'a') as f:
-        f.write('[{}]:{}\n'.format(dt_string, logTxt))  # write log to file
+        f.write('[{}]:{}\n'.format(dt_string, logTxt))
 
 
+# форматуємо дані в "день місяць рік"
 def convert_date(timestamp):
-    d = datetime.utcfromtimestamp(timestamp)
-    formated_date = d.strftime('%d %b %Y')  # format data to "day mounth year"
+    day = datetime.utcfromtimestamp(timestamp)
+    formated_date = day.strftime('%d %b %Y')
     return formated_date
 
 
-def find_files():
-    files = glob.glob(r'./**/*.dat', recursive=True)  # find all .dat files in current directory or in sub directories
-    log("Found {} files:".format(len(files)) + "".join(" " + x for x in files))
+# знаходимо всі файли .dat у поточному каталозі чи підкаталогах
+def find_files_with_dat():
+    files = glob.glob(r'./**/*.dat', recursive=True)
+    get_day_and_write_to_log_file("Found {} files:".format(len(files)) + "".join(" " + x for x in files))
     return files
 
 
-def get_names(files):
+# отримуємо назви файлів
+def get_names_of_files(files):
     names = []
     for x in files:
-        names.append(os.path.split(x)[-1])  # get names of files
-    log("Got file names")
+        names.append(os.path.split(x)[-1])
+    get_day_and_write_to_log_file("Get file names")
     return names
 
 
-def get_fullpath(path):
-    full_path = os.path.abspath(path)  # get full path to the file
-    log("Got full path to file - " + full_path)
+# отримуємо повний шлях до файлу
+def get_fullpath_of_file(path):
+    full_path = os.path.abspath(path)
+    get_day_and_write_to_log_file("Get full path to file - " + full_path)
     return full_path
 
 
+# читаємо всі дані з файлу
 def get_filecontent(path):
     text = str()
     with open(path, 'r') as f:
-        text = f.read()  # read all data from file
-    log("Got content of file - " + path)
+        text = f.read()
+    get_day_and_write_to_log_file("Get content of file - " + path)
     return text
 
 
+# отримуємо розмір файлу в байтах та час створення файлу
 def get_fileinfo(path):
-    file_data = {'size': os.path.getsize(path),  # get file size in bytes
-                 'creation_time': convert_date(os.path.getctime(path))}  # get creation time of the file
-    log("Got info about file - " + path)
+    file_data = {'size': os.path.getsize(path),
+                 'creation_time': convert_date(os.path.getctime(path))}
+    get_day_and_write_to_log_file("Got info about file - " + path)
     return file_data
 
 
-def calc(path, exception_digit=0):
+# обчислюємо суму чисел у файлі
+def calculation(path, exception_digit=0):
     sm = 0
-    log("The calculation of the sum has started in the file " + path)
-    for x in get_filecontent(path).split():  # calculate sum of numbers in file
+    get_day_and_write_to_log_file("The calculation of the sum has started in the file " + path)
+    for x in get_filecontent(path).split():
         try:
             sm += int(x)
         except:
-            log("Сorrupted data {} was found and replaced with {}".format(x, exception_digit))
-            sm += exception_digit  # replace corrupted naumber with exception_digit
-    log("Sum has been calculated")
+            get_day_and_write_to_log_file("Corrupted data {} was found and replaced with {}".format(x, exception_digit))
+            sm += exception_digit
+    get_day_and_write_to_log_file("Sum has been calculated")
     return sm
 
 
 def main():
-    log("Program has started")
-    files = find_files()  # find all .dat files
+    get_day_and_write_to_log_file("Program has started")
+    files = find_files_with_dat()
     subst_num = 0
     print('Choose file: ')
     i = 1
-    for x in get_names(files):
+    for x in get_names_of_files(files):
         print("{} - {}".format(i, x))
         i += 1
     print('{} - settings'.format(i))
-    inp = int(input())  # select option
-    while inp == i:  # open setting dialog
-        subs_num = int(input("Chose number to replace corrupted data: "))  # set new number for substitution
-        log('Replacing number was changed to {}'.format(subs_num))
-        inp = int(input("Chose file: "))  # select option
+    inp = int(input())
+    while inp == i:
+        subs_num = int(input("Chose number to replace corrupted data: "))
+        get_day_and_write_to_log_file('Replacing number was changed to {}'.format(subs_num))
+        inp = int(input("Chose file: "))
         try:
-            chosen_file = files[inp - 1]  # select file
+            chosen_file = files[inp - 1]
         except:
-            chosen_file = files[-1]  # select last file in case of exception
+            chosen_file = files[-1]
     else:
         try:
-            chosen_file = files[inp - 1]  # select file
+            chosen_file = files[inp - 1]
         except:
-            chosen_file = files[-1]  # select last file in case of exception
-    log('File {} chosen'.format(chosen_file))
-    print("Full path to file: \n\t{}".format(get_fullpath(chosen_file)))  # show full path to the file
+            chosen_file = files[-1]
+    get_day_and_write_to_log_file('File {} chosen'.format(chosen_file))
+    print("Full path to file: \n\t{}".format(get_fullpath_of_file(chosen_file)))
     file_inf = get_fileinfo(chosen_file)
     print("File creation time: {}\nFile size: {} bytes".format(file_inf['creation_time'],
-                                                               file_inf['size']))  # show info about the file
-    print("File content:\n\t{}".format(get_filecontent(chosen_file)))  # show file content
-    print("The sum of numbers in the file: {}".format(calc(chosen_file, subst_num)))  # show sum of numbers in file
+                                                               file_inf['size']))
+    print("File content:\n\t{}".format(get_filecontent(chosen_file)))
+    print(
+        "The sum of numbers in the file: {}".format(calculation(chosen_file, subst_num)))  # show sum of numbers in file
     input("Press any button to finish")
-    log("Program has finished")
+    get_day_and_write_to_log_file("Program has finished")
 
 
-if __name__ == "__main__": main()
-
+if __name__ == "__main__":
+    main()
